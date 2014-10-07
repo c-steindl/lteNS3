8220bbcf812917cefcbe0c0db8e5eb04bf7bce26
Christoph Steindl <c.steindl@univie.ac.at>
2014-10-07 16:50:06 +0200

    Plotting vertical lines	for handovers.

--- lena_analyse.r
+++ lena_analyse.r
@@ -4,11 +4,20 @@ wd <- getwd()
 for (directory in args) {
 	setwd(directory)
 	print(getwd())
-	files <- c("buffer.log", "linkconfig.txt", "framelist.txt", "controls.log")
+	files <- c("buffer.log", "linkconfig.txt", "framelist.txt", "controls.log", "handover.log")
 	data <- read.table(files[1], sep=" ", dec=".", header=F)
 	contr <- read.table(files[4], sep=" ", dec=".", header=F)
 	quality <- contr[contr[,2] == "qualityChanged",]
 	y_quality <- rep(0, dim(quality)[1])
+  ho_start <- c()
+  ho_stop <- c()
+	if (file.exists(files[5]) && file.info(files[5])$size > 0) {
+	  log_data <- read.table(files[5], sep=" ", dec=".", header=F, fill=T)
+	  ho_start = log_data[log_data[,2] == "HandoverStart", ]
+	  ho_start = ho_start[ho_start[,3] == "UE", ]
+	  ho_stop = log_data[log_data[,2] == "HandoverEndOk", ]
+	  ho_stop = ho_stop[ho_stop[,3] == "UE", ]
+	}
 	# x_min
 	# x_max
 	# y_min
@@ -18,13 +27,17 @@ for (directory in args) {
 	plot(data[,1], data[,2], type="l", main="Buffer", xlab="Simulation Time [s]", ylab="Data [bytes]")
 	# text(quality[,1], y_quality, labels=quality[,8]*100, pos=1, srt=90)
 	# abline(v=quality[,1], lty=2, col="grey50")
+	abline(v=ho_start[,1], col="skyblue")
+	abline(v=ho_stop[,1], col="skyblue")
 	dev.off()
 	
 	png("bw.png", height = 1080, width = 1920)
 	plot(data[,1], ((data[,3]*8*1e-6)/(data[,1]-1)), type="o", main="Bandwidth", xlab="Simulation Time [s]", ylab="BW [mbit/s]")
 	# text(quality[,1], y_quality, labels=quality[,8]*100, pos=1, srt=90)
 	# abline(v=quality[,1], lty=2, col="grey50")
-	dev.off()
+	abline(v=ho_start[,1], col="skyblue")
+	abline(v=ho_stop[,1], col="skyblue")
+  dev.off()
 
 	png("buffer_time.png", height = 1080, width = 1920)
 	plot(data[,1], data[,5], type="l", main="Buffer Time", xlab="Simulation Time [s]", ylab="Data [s]")
@@ -34,21 +47,27 @@ for (directory in args) {
 	lines(contr[,1], contr[,7], col="yellow")
 	# text(quality[,1], y_quality, labels=quality[,8]*100, pos=1, srt=90)
 	# abline(v=quality[,1], lty=2, col="grey50")
-	dev.off()
+	abline(v=ho_start[,1], col="skyblue")
+	abline(v=ho_stop[,1], col="skyblue")
+  dev.off()
 
 	png("play_transm_time.png", height = 1080, width = 1920)
 	plot(data[,1], data[,6], type="l", main="Transmission and Playback Times", xlab="Simulation Time [s]", ylab="Data [s]")
 	lines(data[,1], data[,7], col="blue")
 	# text(quality[,1], y_quality, labels=quality[,8]*100, pos=1, srt=90)
 	# abline(v=quality[,1], lty=2, col="grey50")
-	dev.off()
+	abline(v=ho_start[,1], col="skyblue")
+	abline(v=ho_stop[,1], col="skyblue")
+  dev.off()
 
 	png("play_transm_bytes.png", height = 1080, width = 1920)
 	plot(data[,1], data[,3], type="l", main="Transmission and Playback Bytes", xlab="Simulation Time [s]", ylab="Data [bytes]")
 	lines(data[,1], data[,4], col="blue")
 	# text(quality[,1], y_quality, labels=quality[,8]*100, pos=1, srt=90)
 	# abline(v=quality[,1], lty=2, col="grey50")
-	dev.off()
+	abline(v=ho_start[,1], col="skyblue")
+	abline(v=ho_stop[,1], col="skyblue")
+  dev.off()
 
 	png("play_transm_ratio.png", height = 1080, width = 1920)
 	plot(ecdf(data[,7]/data[,6]), verticals=TRUE, main="Transmission and Playback Ratio", xlab="Played Video/Transmitted Video [%]", ylab="Frequency [%]")
